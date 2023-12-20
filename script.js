112952137c013cd183699b06a6217ef226327e78
AbeNaws <113221023+AbeNaws@users.noreply.github.com>
2023-12-20 16:27:28 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const ghost = document.querySelector('.ghost');
+
+document.addEventListener('mousemove', handleMouseMove);
+
+function handleMouseMove(e) {
+
+  // Get mouse position
+  const x = e.pageX; 
+  const y = e.pageY;
+
+  // Center position 
+  const centerX = window.innerWidth / 2;
+  const centerY = window.innerHeight / 2;  
+  
+  // Calculate distance from center
+  // and scale it down by 10x
+  const distX = (x - centerX) / 20; 
+  const distY = (y - centerY) / 20;
+
+  // Set position using distance 
+  // from center scaled down
+  ghost.style.left = centerX + distX + 'px';
+  ghost.style.top = centerY + distY + 'px';
+
+}
